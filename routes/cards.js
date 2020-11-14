09d60d6b7b752285c7ae09a0dbd8ea1da846c3b8
James Crosslin <jamesmscrosslin@gmail.com>
2020-11-13 17:17:08 -0800

    Added error catching
    Added try catch, conditional, and redirect in case users go crazy with their urls
    
    [Ticket: X]

--- routes/cards.js
+++ routes/cards.js
@@ -6,22 +6,29 @@ const {
   data: { cards }
 } = require("../data/flashcardData.json")
 
-router.get("/:id", (req, res) => {
-  const side = req.query.side ?? "question"
-  const { id } = req.params
-
-  res.locals = {
-    id,
-    side,
-    text: cards[id][side],
-    hint: cards[id].hint
-  }
-  res.render(`card`)
+router.get("/", (req, res) => {
+  let id = Math.floor(Math.random() * cards.length)
+  res.redirect(`/cards/${id}`)
 })
 
-router.get("/", (req, res) => {
-  const id = Math.floor(Math.random() * cards.length)
-  res.redirect(302, `/cards/${id}`)
+router.get("/:id", (req, res) => {
+  try {
+    let { side } = req.query
+    const { id } = req.params
+    if (!side || side.toLowerCase() !== "answer") {
+      side = "question"
+    }
+    res.locals = {
+      id,
+      side,
+      text: cards[id][side],
+      hint: cards[id].hint
+    }
+    res.render(`card`)
+  } catch (err) {
+    console.error(err.message)
+    res.redirect("/cards")
+  }
 })
 
 module.exports = router
